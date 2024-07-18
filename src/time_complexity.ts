489c2f34ebb1ef06839638a262e809fc5b102cf6
Obi Chinedu Frank <frankieetchy@gmail.com>
2024-07-18 11:45:28 +0100

    time compleity

--- src/time_complexity.ts
+++ src/time_complexity.ts
@@ -1,4 +1,26 @@
 export const solution = (arr: number[]): number => {
   const sum = arr.reduce((a, b)=> a+b, 0);
-  return sum
+  const length = arr.length;
+  const obj: {
+    [key: string]: [number, number]
+  } = {};
+
+  for (let i = 0; i < length - 1; i++) {
+    if (i === 0) {
+      const val = arr[i];
+      obj[i] = [val, sum - val];
+    } else {
+      const oldMap = obj[i - 1];
+      const oldVal = oldMap[0];
+      const currentVal = oldVal + arr[i];
+      obj[i] = [currentVal, (sum - currentVal)]
+    }
+  }
+  const diff = [];
+  for (let key in obj) {
+   const tuple = obj[key];
+   const ans = tuple[0] - (tuple[1]);
+   diff.push(Math.abs(ans))
+  }
+  return  Math.min(...diff)
 }
